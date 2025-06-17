b5ed4adc54e6714f9ddbbef7baf72fb5a98329d9
atharvapachunde <atharvapachunde@gmail.com>
2025-06-18 01:31:46 +0530

    Queue Class & Constructor Code Implemented

---
+++ StacksQueues/Main4Q.java
@@ -0,0 +1,7 @@
+public class Main4Q {
+    public static void main(String[] args) {
+
+        Queue myQ = new Queue(1);
+        myQ.printQ();
+    }
+}
---
+++ StacksQueues/Queue.java
@@ -0,0 +1,45 @@
+public class Queue {
+
+    private int length;
+    private Node first;
+    private Node last;
+
+    class Node{
+        int value;
+        Node next;
+
+        public Node(int value){
+            this.value = value;
+        }
+    }
+
+    public Queue(int value){
+        Node newNode = new Node(value);
+        first = newNode;
+        last = newNode;
+        length = 1;
+    }
+
+    public void printQ(){
+        Node temp = first;
+        while(temp!=null){
+            System.out.print(temp.value+ "  ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+
+    public Node getFirst() {
+        return first;
+    }
+
+    public Node getLast() {
+        return last;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+}
