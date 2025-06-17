3ae103a627717ccc5c833dd12d2bb967727233ab
atharvapachunde <atharvapachunde@gmail.com>
2025-06-18 00:57:38 +0530

    Stack Pop Implemented

--- StacksQueues/Main.java
+++ StacksQueues/Main.java
@@ -2,5 +2,14 @@ public class Main {
     public static void main(String[] args) {
 
         Stack myStack = new Stack(1);
+
+        myStack.getTop();
+        myStack.getHeight();
+        myStack.printStack();
+
+        myStack.push(2);
+        myStack.printStack();
+        myStack.getTop();
+        myStack.getHeight();
     }
 }
--- StacksQueues/Stack.java
+++ StacksQueues/Stack.java
@@ -18,4 +18,32 @@ public class Stack {
         top = newNode;
         height = 1;
     }
+
+    public void printStack(){
+        Node temp = top;
+        while(temp!=null){
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+        System.out.println("---------------------------");
+    }
+
+    public void getTop(){
+        System.out.println("Top = "+ top.value);
+    }
+
+    public void getHeight(){
+        System.out.println("Height = "+ height);
+    }
+
+    public void push(int value){
+        Node newNode = new Node(value);
+        if(height==0) {
+            top = newNode;
+        } else{
+          newNode.next = top;
+          top = newNode;
+        }
+        height++;
+    }
 }
