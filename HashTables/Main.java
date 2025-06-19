1ef38358c6814268b76b531435240fb62ad931ae
atharvapachunde <atharvapachunde@gmail.com>
2025-06-19 16:33:46 +0530

    Hash Table Set method implemented

--- HashTables/Main.java
+++ HashTables/Main.java
@@ -5,5 +5,16 @@ public class Main {
         HashTable myHT = new HashTable();
 
         myHT.printList();
+
+        HashTable myHashTable = new HashTable();
+
+        myHashTable.set("nails",100);
+        myHashTable.set("tile",50);
+        myHashTable.set("lumber",80);
+
+        myHashTable.set("bolts",200);
+        myHashTable.set("screw",140);
+
+        myHashTable.printList();
     }
 }
