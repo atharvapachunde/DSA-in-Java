9bcd3c5c61cf37fdec28f3d87ceef155e5576d8c
atharvapachunde <atharvapachunde@gmail.com>
2025-06-25 16:43:50 +0530

    Graph Add Vertex

---
+++ Graphs/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Graph mygraph = new Graph();
+
+        mygraph.addVertex("A");
+
+        mygraph.printGraph();
+    }
+}
