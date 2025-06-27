fb59c607f4475a0771512ed8b4aca4d484fbbb2c
atharvapachunde <atharvapachunde@gmail.com>
2025-06-27 15:16:24 +0530

    Remove Edge Method Implemented

--- Graphs/Main.java
+++ Graphs/Main.java
@@ -5,7 +5,15 @@ public class Main {
 
         mygraph.addVertex("A");
         mygraph.addVertex("B");
+        mygraph.addVertex("C");
+
         mygraph.addEdge("A","B");
+        mygraph.addEdge("A","C");
+        mygraph.addEdge("B","C");
+
+        mygraph.printGraph();
+
+        mygraph.removeEdge("A","B");
 
         mygraph.printGraph();
     }
